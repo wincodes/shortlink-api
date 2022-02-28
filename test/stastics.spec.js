e5c8b6f948c646f7c0bdf0ab6cbf03da583a59cd
wincodes <otokinaodafe@gmail.com>
2022-02-28 16:37:19 +0100

    feat: write test for statistic endpoint

---
+++ test/stastics.spec.js
@@ -0,0 +1,43 @@
+process.env.NODE_ENV = "test";
+
+const { describe, it } = require("mocha");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { assert } = chai;
+const server = require("../index");
+const Chance = require("chance");
+const Shortlink = require("../models/shortlinks");
+const ShortLinkFactory = require("../factories/ShortlinkFactory");
+
+const chance = Chance();
+
+chai.use(chaiHttp);
+
+describe("Get Statistics of a Url", () => {
+  /*
+   * Encoding endpoint
+   */
+
+  after(async () => {
+    // clear created Data
+    await Shortlink.deleteMany();
+  });
+
+
+  it("it should return 200 and valid Json Details", async () => {
+    const shortlink = await ShortLinkFactory.createSingle();
+
+    const res = await chai.request(server).get(`/api/statistic/${shortlink}`);
+
+    console.log(res.body)
+
+    assert.equal(res.status, 200);
+    assert.equal(res.body.status, "success");
+    assert.equal(res.body.message, "Url Decoded Successfully");
+    assert.isOk(res.body.data.url);
+    assert.isOk(res.body.data.shortlink);
+    assert.equal(res.body.data.shortlink, shortlink);
+    assert.isOk(res.body.data.createdAt);
+    assert.isOk(res.body.data.updatedAt);
+  });
+});
